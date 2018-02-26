c2c70826b6022cd8e83d31ad831c754dadaed446
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2018-02-26 21:39:47 +0100

    Scripts

---
+++ scripts/dotnet-build-all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+REPOSITORIES=(DNC-DShop.Api DNC-DShop.Common DNC-DShop.Messages DNC-DShop.Services.Customers DNC-DShop.Services.Identity DNC-DShop.Services.Notifications DNC-DShop.Services.Operations DNC-DShop.Services.Orders DNC-DShop.Services.Products DNC-DShop.Services.Signalr DNC-DShop.Services.Storage)
+for REPOSITORY in ${REPOSITORIES[*]}
+do
+	 echo ========================================================
+	 echo Building solution: $REPOSITORY
+	 echo ========================================================
+     cd $REPOSITORY
+     git checkout develop
+     dotnet build
+     cd ..
+done
---
+++ scripts/start-infrastructure.sh
@@ -0,0 +1,2 @@
+docker-compose run -d start_dependencies
+docker-compose -f docker-compose-infrastructure.yaml up
