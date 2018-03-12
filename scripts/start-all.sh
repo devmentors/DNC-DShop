a02555eb061f4647a9d813f52d4ec81163ccb308
Piotr Gankiewicz <spetz90@gmail.com>
2018-03-12 09:31:14 +0100

    Start local script

---
+++ scripts/start-all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export ASPNETCORE_ENVIRONMENT=local
+START_LOCAL=./scripts/start.sh
+PREFIX=DNC-DShop
+SERVICE=$PREFIX.Services
+REPOSITORIES=($PREFIX.Api $SERVICE.Customers $SERVICE.Identity $SERVICE.Notifications $SERVICE.Operations $SERVICE.Orders $SERVICE.Products $SERVICE.Signalr $SERVICE.Storage)
+
+for REPOSITORY in ${REPOSITORIES[*]}
+do
+	 echo ========================================================
+	 echo Starting a service: $REPOSITORY
+	 echo ========================================================
+     cd $REPOSITORY
+     $START_LOCAL &
+     cd ..
+done
