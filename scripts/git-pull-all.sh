23d3b4b04ea998dbfc781463cff4c197f736cee9
Pablo Vivera <pvivera@deloitte.com>
2018-12-06 15:01:32 -0500

    Added parameter in git-clone and git-pull for parallelism
    (cherry picked from commit 9c3ee80f3a46057ef744723ba7532200839f40d5)

--- scripts/git-pull-all.sh
+++ scripts/git-pull-all.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 REPOSITORIES=(DNC-DShop DNC-DShop.Api DNC-DShop.Api.Next DNC-DShop.Common DNC-DShop.Services.Customers DNC-DShop.Services.Identity DNC-DShop.Services.Notifications DNC-DShop.Services.Operations DNC-DShop.Services.Orders DNC-DShop.Services.Products DNC-DShop.Services.Signalr)
-for REPOSITORY in ${REPOSITORIES[*]}
-do
-	 echo ========================================================
-	 echo Updating repository: $REPOSITORY
-	 echo ========================================================
-     cd $REPOSITORY
-     git checkout develop
-     git pull
-     git checkout master
-     git pull
-     git checkout develop
-     cd ..
-done
+
+if [ "$1" = "-p" ]
+  then
+    echo ${REPOSITORIES[@]} | sed -E -e 's/[[:blank:]]+/\n/g' | xargs -I {} -n 1 -P 0 sh -c 'printf "========================================================\nUpdating repository: {}\n========================================================\n"; git -C {} checkout develop; git -C {} pull; git -C {} checkout master; git -C {} pull;git -C {} checkout develop;'
+  else
+    for REPOSITORY in ${REPOSITORIES[*]}
+    do
+      echo ========================================================
+      echo Updating repository: $REPOSITORY
+      echo ========================================================
+      cd $REPOSITORY
+      git checkout develop
+      git pull
+      git checkout master
+      git pull
+      git checkout develop
+      cd ..
+    done
+fi
